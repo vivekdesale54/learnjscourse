da65c3ea3ff3dfa45ff9f879d5abdb0003f9a42d
vivek Desale <60285597+vivekdesale54@users.noreply.github.com>
2025-04-20 13:55:59 +0530

    array object

--- array.js
+++ array.js
@@ -22,3 +22,23 @@ const videos = [
 const arrowVideos = videos.map(video => (video.length < 10 ? video : "nope" ));
 
 console.log(arrowVideos);
+
+
+const numbers = [93, 45, 63, 47, 29, 44, 89, 83, 33, 32]
+
+const newNumbers = [...numbers];
+
+newNumbers.sort((a, b) => b - a);
+
+console.log(newNumbers);
+console.log(numbers);
+
+
+const names = ["john", "jane", "doe", "smith", "jones"];
+const otherNames = ["mike", "harvey", "rachel", "jessica", "karen"];
+const allNames = names.concat(otherNames);
+// console.log(allNames);
+
+const allNames2 = [...names, ...otherNames];
+console.log(allNames2); 
+
--- object.js
+++ object.js
@@ -5,3 +5,27 @@
 // };
 
 // console.log(hello);
+
+// sayHi();
+
+// function sayHi () {
+//     console.log("hi");
+// }
+
+const user = {
+    name: "Vivek", 
+};
+
+const admin = {
+    name: "admin",
+}; 
+
+function sayHi() {
+    console.log(this);
+}
+
+user.name = sayHi;
+admin.name = sayHi;
+
+user.sayHi();
+admin.sayHi();
